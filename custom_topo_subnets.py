aa58da4c53fe8c44b65e55dcdc04baf4ae286ebe
hawwwdy <136732843+hawwwdy@users.noreply.github.com>
2025-06-22 19:46:06 +0200

    Update custom_topo_subnets.py

--- custom_topo_subnets.py
+++ custom_topo_subnets.py
@@ -10,11 +10,12 @@ class SDNFirewallTopo(Topo):
         h1 = self.addHost('h1', ip='10.0.1.1/16')  # interner Client
         h2 = self.addHost('h2', ip='10.0.2.2/16')  # Server
         h3 = self.addHost('h3', ip='10.0.3.3/16')  # externer Client
-        h3 = self.addHost('h4', ip='10.0.2.4/16')  # Client in DMZ
+        h4 = self.addHost('h4', ip='10.0.2.4/16')  # Client in DMZ
 
         # Links
         self.addLink(h1, s1)
         self.addLink(h2, s1)
         self.addLink(h3, s1)
+        self.addLink(h4, s1)
 
 topos = { 'sdnfirewall': (lambda: SDNFirewallTopo()) }
