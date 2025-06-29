f37b5a65f16ee32b0c14d8bd2f49b0ab26b9f660
hawwwdy <136732843+hawwwdy@users.noreply.github.com>
2025-06-29 18:05:11 +0200

    hteah

--- custom_topo_subnets.py
+++ custom_topo_subnets.py
@@ -1,3 +1,7 @@
+# Mininet-Topologie fuer Aufagbe B: SDN-Firewall mit statischer ACL
+# Cients in verschiedenen /24 Subnetzen 
+# (Netzmaske /16, damit kein Routing notwendig ist)
+
 from mininet.topo import Topo
 
 class SDNFirewallTopo(Topo):
@@ -8,7 +12,7 @@ class SDNFirewallTopo(Topo):
         # Hosts
         # Netzmaske /16 damit sie ohne Routing kommunizieren können
         h1 = self.addHost('h1', ip='10.0.1.1/16')  # interner Client
-        h2 = self.addHost('h2', ip='10.0.2.2/16')  # Server
+        h2 = self.addHost('h2', ip='10.0.2.2/16')  # Server in DMZ
         h3 = self.addHost('h3', ip='10.0.3.3/16')  # externer Client
         h4 = self.addHost('h4', ip='10.0.2.4/16')  # Client in DMZ
 
